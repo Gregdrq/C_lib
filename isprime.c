ce02066a430915d52b206c6fbc91dc866e22a7c4
Gregdrq <gbuchanan719@gmail.com>
2024-10-27 20:30:58 -0400

    Create isprime.c
    C program to determine if a given number is prime.

---
+++ isprime.c
@@ -0,0 +1,10 @@
+/* isprime.c: determines if an entered number is prime.*/
+int main(char **argv, int argc)
+{
+    int input_num;
+    fscanf("enter a number", %d, inputing);
+    while (!isprime(input_num))
+        isprime(input_num);
+}
+isprime(input_num) {
+    if isint(input_num/2)
